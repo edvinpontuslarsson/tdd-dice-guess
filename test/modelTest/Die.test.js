d33080f4fad53a6993eacd00105caf70fe01acc8
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-17 14:19:01 +0100

    RED: new Die().faceValue should be null

--- test/modelTest/Die.test.js
+++ test/modelTest/Die.test.js
@@ -3,12 +3,17 @@
 const assert = require('chai').assert
 const Die = require('../../src/model/Die')
 
-// https://devhints.io/chai
-
 describe('Test of Die instance', () => {
     describe('Tests that Die can be instantiated', () => {
         it('new Die() should be defined', done => {
             assert.isDefined(new Die())
+            done()
+        })
+    })
+
+    describe('Tests of Die.faceValue', () => {
+        it('new Die().faceValue should be null', done => {
+            assert.isNull(new Die().faceValue)
             done()
         })
     })
