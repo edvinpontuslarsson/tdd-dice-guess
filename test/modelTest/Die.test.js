2e67b580dbf330e8edae39b488650e09d0b1d4bd
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-17 20:37:44 +0100

    RED: Tests of Die.roll(), now die.getFaceValue() should be >= 1 but is 0

--- test/modelTest/Die.test.js
+++ test/modelTest/Die.test.js
@@ -13,21 +13,18 @@ describe('Tests of Die instance', () => {
 
         it('new Die() should be defined', done => {
             const actual = new Die()
-
             assert.isDefined(actual)
             done()
         })
         
         it('new Die().faceValue should be undefined since private', done => {
             const actual = new Die()._faceValue
-            
             assert.isUndefined(actual)
             done()
         })
         
         it('new Die().getFaceValue() should be null', done => {
             const actual = new Die().getFaceValue()
-
             assert.isNull(actual)
             done()
         })
@@ -35,12 +32,21 @@ describe('Tests of Die instance', () => {
 
     describe('Tests of Die.roll()', () => {
         it('die.getFaceValue() should now not be null', done => {
-            const die = new Die()
-            die.roll()
-            const actual = die.getFaceValue()
-
+            const actual = getFaceValueFromNewDieRoll()
             assert.isNotNull(actual)
+            done()
+        })
+
+        it('die.getFaceValue() should be >= 1', done => {
+            const actual = getFaceValueFromNewDieRoll()
+            assert.operator(actual, '>=', 1)
             done()
         })
     })
 })
+
+function getFaceValueFromNewDieRoll() {
+    const die = new Die()
+    die.roll()
+    return die.getFaceValue()
+}
