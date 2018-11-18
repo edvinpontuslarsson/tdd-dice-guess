53fd671b98fe9f6cf1666d505a66c8be3589b0a0
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-18 08:19:14 +0100

    REFACTORING: removes maxvalue param from Die instance

--- src/model/Die.js
+++ src/model/Die.js
@@ -1,10 +1,9 @@
 'use strict'
 
 export default class Die {
-    constructor(maxValue = 6) {
+    constructor() {
         // babel plugin makes these attributes private
         this._faceValue = null
-        this._maxValue = maxValue
     }
 
     getFaceValue() { return this._faceValue }
