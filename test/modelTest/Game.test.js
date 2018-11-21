c6b0c3a387d4b485ce523d01881a90de1af2c47e
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-21 19:47:54 +0100

    RED: new Game() should throw EmptyArgumentError

--- test/modelTest/Game.test.js
+++ test/modelTest/Game.test.js
@@ -4,12 +4,18 @@ const Game = require('../../src/model/Game')
 const CustomError = require('../../src/model/CustomError')
 
 describe('Tests of Game instance', () => {
-  describe('Tests of game.isGuessCorrect()', () => {
+  describe('Tests of new Game()', () => {
+    it('new Game() should throw EmptyArgumentError', () => {
+      expect(() => new Game())
+        .toThrowError(CustomError.EmptyArgumentError)
+    })
+  })
+  describe('Tests of new Game().isGuessCorrect()', () => {
     it('new Game().isGuessCorrect() should throw EmptyArgumentError', () => {
       expect(() => new Game().isGuessCorrect())
         .toThrowError(CustomError.EmptyArgumentError)
     })
-    // it('new Game(dieMock) should create new dieMock instance', () => {
+    // it('new Game(dieMock) should call dieMock constructor', () => {
         
     // })
   })
