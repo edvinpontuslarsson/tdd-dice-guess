acc3cdc72116538f1f5e519136f2dac0c3c5707f
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-21 19:50:01 +0100

    GREEN: new Game() should throw EmptyArgumentError

--- src/model/Game.js
+++ src/model/Game.js
@@ -3,14 +3,18 @@
 const CustomError = require('./CustomError')
 
 class Game {
+  constructor(die) {
+    if (!die) throw new CustomError.EmptyArgumentError()
+    
+    this.die = die
+  }
+
   isGuessCorrect (guess) {
     this.validateGuess(guess)
   }
 
   validateGuess (guess) {
-    if (!guess) {
-      throw new CustomError.EmptyArgumentError()
-    }
+    if (!guess) throw new CustomError.EmptyArgumentError()
   }
 }
 
