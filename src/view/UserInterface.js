53c7865bfb4768beab6714cf76101c53533c7927
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-23 15:43:16 +0100

    REFACTORING: puts all UserInterface methods in constructor for consistency

--- src/view/UserInterface.js
+++ src/view/UserInterface.js
@@ -6,12 +6,12 @@ class UserInterface {
             const amount = game.getRolledDiceAmount()
             console.log(`Amount of dice rolled: ${amount}`)
         }
-    }
 
-    displayInstructions() {
-        console.log(
-            'Enter "r" to roll another die or enter an integer to guess total dice value'
-        )
+        this.displayInstructions = () => {
+            console.log(
+                'Enter "r" to roll another die or enter an integer to guess total dice value'
+            )
+        }
     }
 }
 
