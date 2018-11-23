00b60ebeec41169d50725495a88179fcc08bd74a
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-23 15:06:27 +0100

    Tests of UserInterface › Test of displayInstructions › Should call console.log with correct message

--- test/viewTest/UserInterface.test.js
+++ test/viewTest/UserInterface.test.js
@@ -2,19 +2,23 @@
 
 const UI = require('../../src/view/UserInterface')
 const mockConsole = require('jest-mock-console').default
+const Game = require('../../src/model/Game')
+const Die = require('../../src/model/Die')
 
 // start with welcome message
 describe('Tests of UserInterface', () => {
 
-    describe('Test of displayWelcomeMessage', () => {
-        it('Should call console.log with "Welcome!"', () => {
-            
+    describe('Test of displayInstructions', () => {
+        it('Should call console.log with correct message', () => {
             const restoreConsole = mockConsole()
             
-            new UI().displayWelcomeMessage()
+            const game = new Game(Die)
+            new UI(game).displayInstructions()
             
             expect(console.log)
-                .toHaveBeenCalledWith('Welcome!')
+                .toHaveBeenCalledWith(
+                    'Enter "r" to roll another die or enter an integer to guess total dice value'
+                )
             restoreConsole()
         })
     })
