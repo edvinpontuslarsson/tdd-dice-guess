ec66f35ab7ae90b9aa8b0c7dab5ee602bbd69d59
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-23 12:34:13 +0100

    REFACTORING: updates index test file a bit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const run = () => {
-
+    
 }
 
 module.exports = run
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const index = require('../src/index')
+const Game = require('../src/model/Game')
+const Die = require('../src/model/Die')
 
 describe('Smoke tests of index file', () => {
     it('index should be defined & run without errors', () => {
@@ -8,3 +10,7 @@ describe('Smoke tests of index file', () => {
         expect(index).toBeDefined()
     })
 })
+
+// mock game, see if constructs, should make game
+
+
