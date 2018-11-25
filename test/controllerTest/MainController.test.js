a5e6b3a348b2654f09dbf24ee3040caeff5bd8d5
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-25 06:20:32 +0100

    RED: new Controller().playGame() should throw EmptyArgumentError

--- test/controllerTest/MainController.test.js
+++ test/controllerTest/MainController.test.js
@@ -1,15 +1,19 @@
 'use strict'
 
-const MainController = require('../../src/controller/MainController')
+const Controller = require('../../src/controller/Controller')
+const CustomError = require('../../src/model/CustomError')
 const Game = require('../../src/model/Game')
 const Die = require('../../src/model/Die')
 const UserInterface = require('../../src/view/UserInterface')
 const ReadlineSyncStub = require('../viewTest/ReadlineSyncStub')
-/*
-describe('Tests of MainController instance', () => {
+
+describe('Tests of Controller instance', () => {
 
     // should throw empty arg errs, mock & call
-
-    describe('Tests of playGame')
+    describe('Tests of playGame', () => {
+        it('new Controller().playGame() should throw EmptyArgumentError', () => {
+            expect(() => new Controller())
+                .toThrowError(CustomError.EmptyArgumentError)
+        })
+    })
 })
-*/
