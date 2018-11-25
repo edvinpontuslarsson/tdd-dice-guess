64c0c3dd3a52522ff4cc160b8c97796678d71d0f
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-25 18:52:42 +0100

    RED: Tests of index file › Should call UserInterface constructor

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 
 const Game = require('./model/Game')
 const Die = require('./model/Die')
+const UserInterface = require('./view/UserInterface')
+const readlineSync = require('readline-sync')
 
 const run = () => {
     const game = new Game(new Die())
+    const ui = new UserInterface(game, readlineSync)
 }
 
 module.exports = { run }
