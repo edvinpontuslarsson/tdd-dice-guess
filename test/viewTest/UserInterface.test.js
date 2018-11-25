79d1bde5246146e68b056b2362f9c75309500a06
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-25 06:35:37 +0100

    RED: didUserGuess › Should return false if user input is a negative integer

--- test/viewTest/UserInterface.test.js
+++ test/viewTest/UserInterface.test.js
@@ -157,6 +157,15 @@ describe('Tests of UserInterface instance', () => {
             const actual = ui.didUserGuess()
             expect(actual).toBe(false)
         })
+
+        it('Should return false if user input is a negative integer', () => {
+            const game = new Game(new Die())
+            const simpleReadlineStub = { question: () => -1 }
+            const ui = new UserInterface(game, simpleReadlineStub)
+
+            const actual = ui.didUserGuess()
+            expect(actual).toBe(false)
+        })
     })
 
     describe('Test of rectifyUser', () => {
