56225b65dcb29551b17069c84095da60687fc15b
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-27 17:09:25 +0100

    REFACTORING: deletes commented unused code

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -5,7 +5,7 @@ const CustomError = require('../../src/model/CustomError')
 class Controller {
     playGame(game, ui) {
         if (!game || !ui) throw new CustomError.EmptyArgumentError()
-        // game.rollNewDie()
+
         ui.initializeView(console)
         ui.displayRolledDiceAmount()
         ui.displayInstructions()
