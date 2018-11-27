994198e4c982fe77081cffcc18975322f11f2c5b
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-27 08:11:57 +0100

    GREEN: index file › Should call Controller constructor

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const Game = require('./model/Game')
 const Die = require('./model/Die')
 const UserInterface = require('./view/UserInterface')
 const readlineSync = require('readline-sync')
+const Controller = require('./controller/Controller')
 
 const run = () => {
     const game = new Game(new Die())
     const ui = new UserInterface(game, readlineSync)
+    const controller = new Controller()
 }
 
 module.exports = { run }
