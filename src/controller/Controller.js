bf55f44839d9d4742c2ce89b81c53ffdb5b3fa53
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-11-28 08:51:01 +0100

    REFACTORING: cleans code, splits functions

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -4,24 +4,29 @@ const CustomError = require('../../src/model/CustomError')
 
 class Controller {
     playGame(game, ui) {
-        if (!game || !ui) throw new CustomError.EmptyArgumentError()
+        if (!game || !ui) 
+            throw new CustomError.EmptyArgumentError()
 
         ui.initializeView(console)
         ui.displayRolledDiceAmount()
         ui.displayInstructions()
 
-        if (ui.doesUserWantToRollNewDie()) game.rollNewDie()
+        if (ui.doesUserWantToRollNewDie()) 
+            game.rollNewDie()
 
-        if (ui.didUserGuess()) {
-            const guess = ui.getGuess()
+        if (ui.didUserGuess())
+            this.handleGuess(game, ui)
+    }
+
+    handleGuess(game, ui) {
+        const guess = ui.getGuess()
             
-            if (game.isGuessCorrect(guess))
-                ui.displayCorrectGuess()
-            else
-                ui.displayIncorrectGuess()
+        if (game.isGuessCorrect(guess))
+            ui.displayCorrectGuess()
+        else
+            ui.displayIncorrectGuess()
 
-            game.resetGame()
-        }
+        game.resetGame()
     }
 }
 
